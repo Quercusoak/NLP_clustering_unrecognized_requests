3540030f601f7958f5ffa24f0559fdc1f6492b89
alona <a@a>
2025-02-18 23:16:43 +0200

    broke into methods

--- main.py
+++ main.py
@@ -14,7 +14,62 @@ def extract_cluster_name(requests):
     return " ".join(common_words)
 
 
-def create_clusters(requests, model, num_representatives, min_size):
+def init_clusters(embeddings, min_size):
+    """Use DBSCAN to initialize clusters, calculate their centroids, and update cluster labels"""
+
+    # Get initial clusters from DBSCAN
+    dbscan = DBSCAN(eps=0.3, min_samples=min_size)
+    labels = dbscan.fit_predict(embeddings)
+
+    # Initialize incremental clusters
+    clusters = {}
+    cluster_assignments = labels.tolist()
+
+    for i, label in enumerate(labels):
+        if label != -1:
+            clusters.setdefault(label, []).append(embeddings[i])
+
+    centroids = {label: np.mean(clusters[label], axis=0) for label in clusters}
+
+    return clusters, centroids, cluster_assignments
+
+
+def add_embedding_to_cluster(model, cluster_assignments, embeddings, clusters, centroids):
+    threshold = 0.65
+
+    for i, embedding in enumerate(embeddings):
+        # Get cosine similarity between embedded request and centroids
+        similarities = model.similarity(embedding, list(centroids.values()))[0]
+        max_index = np.argmax(similarities).item()  # index of the closest cluster
+        
+        # If embedded request is close to existing cluster - add and recalculate centroid
+        if similarities[max_index] >= threshold:
+            cluster_label = int(list(centroids.keys())[max_index])
+            clusters[cluster_label].append(embedding)
+            centroids[cluster_label] = np.mean(clusters[cluster_label], axis=0)
+        else:
+            cluster_label = int(max(clusters.keys(), default=-1) + 1)
+            clusters[cluster_label] = [embedding]
+            centroids[cluster_label] = embedding
+
+        # Update embedding's cluster label
+        cluster_assignments[i] = cluster_label
+    
+
+def remove_small_clusters(cluster_assignments, clusters, centroids, min_size):
+    """remove clusters with less than min_size members"""
+    to_remove = [label for label, cluster in clusters.items() if len(cluster) < min_size]
+
+    for i, label in enumerate(cluster_assignments):
+        if label in to_remove:
+            cluster_assignments[i] = -1
+
+    for i in to_remove:
+        clusters.pop(i)
+        centroids.pop(i)
+
+
+def create_clusters(requests, min_size):
     """
     generate clusters and centroids:
     iterate over embeddings, where each request can be assigned to an existing cluster
@@ -24,6 +79,38 @@ def create_clusters(requests, model, num_representatives, min_size):
     perform additional iterations over all request embeddings, re-assigning them if needed
     • till the algo convergence or till the max number of iterations is exhausted
     """
+
+    # encode a set of unhandled requests using the sentence-transformers library
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = model.encode(requests)
+
+    # Use DBSCAN to initialize clusters, and calculate their centroids
+    clusters, centroids, cluster_assignments = init_clusters(embeddings, min_size)
+
+    # Incremental clustering refinement
+    max_iterations = 10
+    for iteration in range(max_iterations):
+        old_assignments = cluster_assignments.copy()
+
+        print(f'Iteration {iteration} start: {len(clusters)} clusters')
+
+        # Iterate over embeddings and add them to clusters
+        add_embedding_to_cluster(model, cluster_assignments, embeddings, clusters, centroids)
+
+        # remove clusters with less than min_size members
+        remove_small_clusters(cluster_assignments, clusters, centroids, min_size)
+
+        print(f"Iteration {iteration} end: {len(clusters)} clusters")
+
+        # early stopping: if clusters aren't changed or added
+        max_changes = 5
+        num_changes = sum(1 for a, b in zip(old_assignments, cluster_assignments) if a != b)
+        print(f'num changes = {num_changes}')
+        if num_changes <= max_changes:
+            print(f"Early stop after {iteration} iterations")
+            break
+
+    return cluster_assignments
 
 
 def analyze_unrecognized_requests(data_file, output_file, num_representatives, min_size):
@@ -40,69 +127,7 @@ def analyze_unrecognized_requests(data_file, output_file, num_representatives, m
     min_size = int(min_size)
     num_representatives = int(num_representatives)
 
-    # encode a set of unhandled requests using the sentence-transformers library
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-    embeddings = model.encode(requests)
-
-    threshold = 0.65
-    dbscan = DBSCAN(eps=0.3, min_samples=min_size)
-    labels = dbscan.fit_predict(embeddings)
-
-    # Initialize incremental clusters
-    clusters = {}
-    cluster_assignments = labels.tolist()
-
-    for i, label in enumerate(labels):
-        if label != -1:
-            clusters.setdefault(label, []).append(embeddings[i])
-
-    centroids = {label: np.mean(clusters[label], axis=0) for label in clusters}
-
-    # Incremental clustering refinement
-    max_iterations = 10
-    for iteration in range(max_iterations):
-        old_assignments = cluster_assignments.copy()
-
-        print(f'Iteration {iteration} start: {len(clusters)} clusters')
-
-        for i, embedding in enumerate(embeddings):
-
-            # Get cosine similarity between embedded request and centroids
-            similarities = model.similarity(embedding, list(centroids.values()))[0]
-            max_index = np.argmax(similarities).item()  # index of the closest cluster
-
-            # If embedded request is close to existing cluster - add and recalculate centroid
-            if similarities[max_index] >= threshold:
-                cluster_label = int(list(centroids.keys())[max_index])
-                clusters[cluster_label].append(embedding)
-                centroids[cluster_label] = np.mean(clusters[cluster_label], axis=0)
-            else:
-                cluster_label = int(max(clusters.keys(), default=-1) + 1)
-                clusters[cluster_label] = [embedding]
-                centroids[cluster_label] = embedding
-
-            cluster_assignments[i] = cluster_label
-
-        # remove clusters with less than min_size members
-        to_remove = [label for label, cluster in clusters.items() if len(cluster) < min_size]
-
-        for i, label in enumerate(cluster_assignments):
-            if label in to_remove:
-                cluster_assignments[i] = -1
-
-        for i in to_remove:
-            clusters.pop(i)
-            centroids.pop(i)
-
-        print(f"Iteration {iteration} end: {len(clusters)} clusters")
-
-        # early stopping: if clusters aren't changed or added
-        max_changes = 5
-        num_changes = sum(1 for a, b in zip(old_assignments, cluster_assignments) if a != b)
-        print(f'num changes = {num_changes}')
-        if num_changes <= max_changes:
-            print(f"Early stop after {iteration} iterations")
-            break
+    cluster_assignments = create_clusters(requests, min_size)
 
     final_clusters = {}
     for i, cluster_id in enumerate(cluster_assignments):
